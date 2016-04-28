3760004e060d9a35aeb234993ffefa16cf033b13
grozner <grozner@yahoo.com>
2016-04-28 14:32:23 -0400

    Added files via upload

---
+++ HelloSpring/src/com/tutorialspoint/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.tutorialspoint;
+
+public class HelloWorld {
+   private String exampleofmessage;
+
+   public void setGilmsg(String message2){
+      exampleofmessage  = message2;
+   }
+
+   public void getGilmsg(){
+      System.out.println("My Message : " + exampleofmessage);
+   }
+}
---
+++ HelloSpring/src/com/tutorialspoint/MyOtherMessage.java
@@ -0,0 +1,14 @@
+package com.tutorialspoint;
+
+public class MyOtherMessage {
+   private String exampleofmessage2;
+
+   public void setGilmsg2(String message2){
+      exampleofmessage2  = message2;
+   }
+
+   public void getGilmsg2(){
+      System.out.println("My Message : " + exampleofmessage2);
+      
+   }
+}
